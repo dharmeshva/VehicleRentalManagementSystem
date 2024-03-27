73b63fd511c1a52db750e6efe34ae8f0e457b3fd
Dharmesh Arvindbhai Vanani <Dvanani46@gmail.com>
2024-03-27 19:54:03 -0400

    add Vehicle.cs

---
+++ VehicleRentalManagementSystem/Vehicle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleRentalManagementSystem
+{
+    internal class Vehicle
+    {
+        // Properties
+        public string Model { get; set; }
+        public string Manufacturer { get; set; }
+        public int Year { get; set; }
+        public decimal RentalPrice { get; set; }
+
+        // Constructor
+        public Vehicle(string model, string manufacturer, int year, decimal rentalPrice)
+        {
+            Model = model;
+            Manufacturer = manufacturer;
+            Year = year;
+            RentalPrice = rentalPrice;
+        }
+
+        // Method to display vehicle details
+        public virtual void DisplayDetails()
+        {
+            Console.WriteLine($"_______________________________________ \n");
+            Console.WriteLine($"Model: {Model}");
+            Console.WriteLine($"Manufacturer: {Manufacturer}");
+            Console.WriteLine($"Year: {Year}");
+            Console.WriteLine($"Rental Price: {RentalPrice}");
+
+        }
+    }
+}
